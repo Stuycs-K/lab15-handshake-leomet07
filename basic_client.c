95a7da1afda90be932ef69b2f98962df71460094
leomet07 <leomet07@gmail.com>
2025-01-08 13:51:36 -0500

    soft exit client on server exit

--- basic_client.c
+++ basic_client.c
@@ -6,15 +6,19 @@ int main() {
 
     from_server = client_handshake(&to_server);
 
-    while (1){
+    while (1) {
         char *msg_from_server = calloc(256, sizeof(char)); // make sure mem is zeroed out to not fight with previous message
 
         int read_hi_status = 0;
-        while (read_hi_status == 0) {
-            read_hi_status = read(from_server, msg_from_server, 256);
-            if (read_hi_status == -1) {
-                err();
-            }
+
+        read_hi_status = read(from_server, msg_from_server, 256);
+        if (read_hi_status == -1) {
+            err();
+        }
+
+        if (read_hi_status == 0) {
+            printf("Pipe was just closed (read zero bytes!)... quitting!\n");
+            exit(0);
         }
         printf("Client successfully read |%s| from server.\n", msg_from_server);
 
